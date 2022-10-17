c5264715aa49ed720acad55df0b308c8d1414b56
Rajya Vardhan <vardhanrajya08@gmail.com>
2022-10-17 23:18:26 +0530

    achitechture created.

---
+++ index.php
@@ -0,0 +1,31 @@
+<?php
+//     contents in php
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Web Shell</title>
+    <style>
+        /* style contentents */
+    </style>
+
+</head>
+
+<body>
+    <main>
+        <h1>Web Shell</h1>
+        <h2>Execute a command</h2>
+
+        <form method="post">
+            <!-- CLI -->
+        </form>
+
+        <?php
+            // script
+        ?>
+    </main>
+</body>
+</html>
